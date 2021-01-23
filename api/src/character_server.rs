5d3595505b6539420d4ad6d20d11d51032d9ce48
Sam De Roeck <sadroeck@gmail.com>
2021-01-23 16:34:53 +0100

    [impl] Connect map server adds name & ip

--- api/src/character_server.rs
+++ api/src/character_server.rs
@@ -1,8 +1,10 @@
+use async_std::net::IpAddr;
+
 #[derive(Debug, thiserror::Error)]
 pub enum ConnectError {}
 
 #[async_trait::async_trait]
 pub trait CharacterServer {
-    async fn connect_map_server(&self) -> Result<(), ConnectError>;
+    async fn connect_map_server(&self, name: &str, ip: IpAddr) -> Result<(), ConnectError>;
     async fn ping(&self) -> Result<(), ConnectError>;
 }
